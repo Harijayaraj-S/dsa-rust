f0f50768b158da3cd9e62c9744dfdfd7fcd737d2
Harijayaraj-S <harijayaraj011@gmail.com>
2024-08-05 07:10:17 +0530

    Updated: leedcode solution with test case

--- dsa_rust/src/basic_array.rs
+++ dsa_rust/src/basic_array.rs
@@ -1,4 +1,3 @@
-
 // leetcode 1920. Build Array from Permutation
 struct Solution;
 impl Solution {
@@ -8,11 +7,27 @@ impl Solution {
     }
 }
 
+// 1929. Concatenation of Array
+struct ConcatenationSolution;
+impl ConcatenationSolution {
+    pub fn get_concatenation(nums: Vec<i32>) -> Vec<i32> {
+        let mut ans: Vec<i32> = Vec::new();
+        for index in 0..(nums.len() * 2) {
+            if index < nums.len() {
+                ans.push(nums[index])
+            } else {
+                ans.push(nums[index - nums.len()])
+            }
+        }
+        ans
+    }
+}
+
 mod tests {
     use super::*;
 
     #[test]
-    fn build_array() {
+    fn test_build_array() {
         assert_eq!(
             Solution::build_array([0, 2, 1, 5, 3, 4].to_vec()),
             [0, 1, 2, 4, 5, 3].to_vec()
@@ -21,6 +36,19 @@ mod tests {
         assert_eq!(
             Solution::build_array([5, 0, 1, 2, 3, 4].to_vec()),
             [4, 5, 0, 1, 2, 3].to_vec()
+        );
+    }
+
+    #[test]
+    fn test_get_concatenation() {
+        assert_eq!(
+            ConcatenationSolution::get_concatenation([1, 2, 1].to_vec()),
+            [1, 2, 1, 1, 2, 1].to_vec()
+        );
+
+        assert_eq!(
+            ConcatenationSolution::get_concatenation([1, 3, 2, 1].to_vec()),
+            [1, 3, 2, 1, 1, 3, 2, 1].to_vec()
         );
     }
 }
